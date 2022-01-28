441eab9de0e44cfa8c03cdc5f02e8390fd953a78
aadil <aadil@promactinfo.com>
2022-01-28 15:37:31 +0530

    completed user module with all excess REST apis for practice

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { CreateUserDTO } from './dtos/create-user.dto';
+import { UpdateUserDTO } from './dtos/update-user.dto';
+import { UsersService } from './users.service';
 
-@Controller('users')
-export class UsersController {}
+@Controller('auth')
+export class UsersController {
+    constructor(private usersService: UsersService) { }
+
+    @Post('/signup')
+    createUser(@Body() body: CreateUserDTO) {
+        console.log(body)
+        this.usersService.create(body.email, body.password)
+    }
+
+    @Get('/:id')
+    findUser(@Param('id') id: string) {
+        return this.usersService.findOne(parseInt(id))
+    }
+
+    @Get()
+    findUserByEmail(@Query('email') email: string) {
+        return this.usersService.find(email)
+    }
+
+    @Delete('/:id')
+    deleteUser(@Param('id') id: string) {
+        return this.usersService.remove(parseInt(id))
+    }
+
+    @Patch('/:id')
+    updateUser(@Param('id') id: string, @Body() body: UpdateUserDTO) {
+        return this.usersService.update(parseInt(id), body)
+    }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,46 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(@InjectRepository(User) private repo: Repository<User>) { }
+    //create user
+    create(email: string, password: string) {
+        const user = this.repo.create({ email, password })
+        return this.repo.save(user)
+
+    }
+    //find user
+    async findOne(id: number) {
+        const user = await this.repo.findOne(id)
+        if (!user) {
+            throw new Error('user not found')
+        }
+        return user
+
+    }
+    async find(email: string) {
+        const user = await this.repo.find({ email })
+        if (!user) {
+            throw new Error('user not found')
+        }
+        return user
+    }
+    async update(id: number, data: Partial<User>) {
+        const user = await this.repo.findOne(id)
+        if (!user) {
+            throw new Error('user not found')
+        }
+        await this.repo.save({ ...user, ...data })
+    }
+    async remove(id: number) {
+        const user = await this.repo.findOne(id)
+        if (!user) {
+            throw new Error('user not found')
+        }
+        return this.repo.remove(user)
+
+    }
+}
